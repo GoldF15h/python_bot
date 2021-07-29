641684608d779dfbdc53509532a64b42d0a8cd5a
GoldF15h <todsapon.singsunjit@gmail.com>
2021-07-29 23:31:52 +0700

    about 300 on AVG score every song ,need to improve speed

--- source/main.py
+++ source/main.py
@@ -1,25 +1,62 @@
-import pyautogui
-import time
-import numpy as np
-import cv2
-import keyboard
-import os
+from pyautogui import click
+from pyautogui import screenshot as sc
+from numpy import array
+from cv2 import cvtColor,COLOR_RGB2GRAY,destroyAllWindows
+from keyboard import is_pressed
+from os import system
+
+# game use in this porject
+# https://poki.com/th/g/piano-tiles-2
+# Top Left      609   ,  120
+# Bottom Right  1021  ,  883
+# SIZE ->       775   X  413
+# old setting (473,0,-50)
+def black_scan_click (screenshot,y_cord,time):
+        # scan for black color
+        for x in range(649,0,-50) :
+            for y in y_cord :
+                # if color is black
+                if screenshot[x,y] < 10 :
+                    click(609+y,109+x+time/5)
+                    return
 
 if __name__ == "__main__" :
-    os.system('cls')
+    system('cls')
+    print("BOT IS STANDBY \nPRESS 'P' TO START")
+    
+    # defind what x coordinate value we need to check
+    y_cord = [50,150,250,350]
+
+    # stand by
+    while not is_pressed('p') :
+       pass
+    
+    system('cls')
     print("BOT IS RUNNING")
 
-    while not keyboard.is_pressed('esc') :
+    # how many time we click
+    time = 0
+
+    # start
+    while not is_pressed('esc') :
         # take Screenshot
-        screenshot = pyautogui.screenshot()
+        screenshot = sc(region=(609,120, 413, 650))
         
         # convert screenshot to CV2 tpye
-        screenshot = np.array(screenshot)
-        screenshot = cv2.cvtColor(screenshot,cv2.COLOR_BGR2GRAY)
-        # show image for debug
-        cv2.imshow('capture',screenshot) 
-        if cv2.waitKey(1) == ord('q'):
-            cv2.destroyAllWindows
-            break
+        screenshot = array(screenshot)
+        screenshot = cvtColor(screenshot,COLOR_RGB2GRAY)
 
-        # time.sleep(2)
+        # find tiles and click on it
+        black_scan_click(screenshot,y_cord,time)
+        time = time + 1
+    
+        # show image pfor debug
+        # cv2.imshow('capture',screenshot) 
+        # if cv2.waitKey(1) == ord('q'):
+        #     break
+
+
+    # end of the program
+    destroyAllWindows
+    system('cls')
+    print("STOP!")
---
+++ source/tmp.py
@@ -0,0 +1,3 @@
+import pyautogui
+
+pyautogui.moveTo(1500,500,0)
